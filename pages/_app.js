85449aee39d38da274cc71906565953fe2002200
luchanso <luchansodev@gmail.com>
2022-11-17 04:10:30 +0500

    feat: init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({ initialColorMode: "dark" });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
